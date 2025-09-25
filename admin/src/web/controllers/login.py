d39e730d367410fca245aaff006e886df54dc85e
Valentin Paolini <valenpaolini@hotmail.com>
2025-09-25 17:21:14 -0300

    Auth service

--- admin/src/web/controllers/login.py
+++ admin/src/web/controllers/login.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, url_for
 from flask import render_template
 from flask import request, session
+from core.services.auth_service import authenticate
 
 login_bp = Blueprint("login", __name__)
 
@@ -9,5 +10,8 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
     else:
-        session["user_id"] = request.form["email"]
-        return redirect(url_for("home", logged_user=session["user_id"]))
+        user = authenticate(request.form("email"), request.form("password"))
+        if user:
+            return redirect(url_for("home", logged_user=user.email))
+        else:
+            return "<h1>no existe</h1>"
