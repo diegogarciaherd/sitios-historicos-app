28a5d9751983378ab1312ef36c85845c0e4eef31
Valentin Paolini <valenpaolini@hotmail.com>
2025-09-26 12:11:06 -0300

    CRUD de usuarios

--- admin/src/core/models/user.py
+++ admin/src/core/models/user.py
@@ -1,6 +1,7 @@
 from src.core.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from core.models.userrole import UserRole as Role
+from core.database import db
 
 class User(Base):
     __tablename__ = "users"
@@ -14,3 +15,31 @@ class User(Base):
 
     def __repr__(self):
         return f"<Usuario {self.id}: {self.email}, {self.name}, {self.last_name}, {self.active}, {self.role}>"
+
+def create_user(**kwargs):
+    email = kwargs["email"]
+    existente = db.session.query(User).filter_by(email=email).first()
+    if existente:
+        return False
+    else:
+        user = User(**kwargs)
+        db.session.add(user)
+        db.session.commit()
+        return True
+
+def read_user_by_email(email):
+    return db.session.query(User).filter_by(email=email).first()
+
+def read_users_by_activeness(active):
+    return db.session.query(User).filter_by(active=active)
+
+def read_users_by_role(role):
+    return db.session.query(User).filter_by(role=role)
+
+def update_value(id, values):
+    db.session.query(User).filter_by(id=id).update(values)
+    db.session.commit()
+
+def delete_user(id):
+    db.session.query(User).filter_by(id=id).delete()
+    db.session.commit()
