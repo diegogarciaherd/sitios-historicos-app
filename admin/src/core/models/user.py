5a34127705ea05be70c0e5f365376c678a2f760d
Augusto Boschiazzo Marinelli <augus2608@gmail.com>
2025-10-27 15:49:12 -0300

    remove userrole

--- admin/src/core/models/user.py
+++ admin/src/core/models/user.py
@@ -1,6 +1,5 @@
 from core.database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from core.models.userrole import UserRole as Role
 from core.database import db
 from core.services.bcrypt import bcrypt
 from core.database import Base
@@ -17,7 +16,6 @@ class User(Base):
     last_name: Mapped[str] = mapped_column(nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     active: Mapped[bool] = mapped_column(nullable=True, default=True)
-    role: Mapped[Role] = mapped_column(nullable=True, default=Role.PUBLIC)
     sys_admin: Mapped[bool] = mapped_column(nullable=True, default=False)    
     feature_flags_history: Mapped["FeatureFlagHistory"] = relationship(
         "FeatureFlagHistory",
