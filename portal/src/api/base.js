cd79e765064bde2ff517f6e80fd399edf940d98a
Nicolas Hermida <nshermida101@gmail.com>
2025-11-19 15:37:50 -0300

    f

--- portal/src/api/base.js
+++ portal/src/api/base.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: 'https://admin-grupo45.proyecto2025.linti.unlp.edu.ar/api',
 })
 
 export default api
